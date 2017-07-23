92c5c043448c8ef8b3d104e92f9dc9af57a59f8f
ashdawngary <ashdawngary@gmail.com>
2017-07-23 19:44:17 -0400

    Testing Async Start Screen for Button Rendering
    Async start screen

--- Pong.lua
+++ Pong.lua
@@ -1,14 +1,15 @@
 -- Pong for LUA
 os.loadAPI("advapi")
-instance = advapi.getMonitorInstance("monitor_0")
+instance = advapi.getMonitorInstance("monitor_5")
 print(instance.getSize())
 mst,mstc,msb,msbu = advapi.createScreen(instance)
 paddle1 = 1
 paddle2 = 1
 function drawTitleScreen()
-	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,10,5,5,20,colors.white,false)
+	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,5,5,20,10,colors.white,false)
+	mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,10,7,"START",colors.green,colors.white)
 	mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,1,1,"Pong By IntelX",colors.blue,colors.black)
-	mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,20,22,"Click the White Button to Start!",colors.blue,colors.black)
+	mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,10,20,"Click the White Button to Start!",colors.blue,colors.black)
 	advapi.updateScreen(instance,mst,mstc,msb)
 end
 function drawScreen(p1,p2,bx,by)
@@ -19,3 +20,20 @@ function drawScreen(p1,p2,bx,by)
 	advapi.updateScreen(instance,mst,mstc,msb)
 	return mst,mstc,msb,msbu
 end
+function waitforstart()
+	
+	while true do
+	local event, side, x, y = os.pullEvent("monitor_touch")
+	print(x.."  and "..y)
+	if x  > 5 and x <= 20 then
+		if y > 10 and y < 20 then
+			print("Start was hit!")
+			return 1
+			
+		end
+	end
+	end
+	
+	
+end
+parallel.waitForAny(drawTitleScreen,waitforstart)
