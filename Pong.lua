21e5ad7b0336df27622288d328c267243634bcf4
ashdawngary <ashdawngary@gmail.com>
2017-07-23 17:07:51 -0400

    fix #1

--- Pong.lua
+++ Pong.lua
@@ -2,20 +2,20 @@
 os.loadAPI("advapi")
 instance = advapi.getMonitorInstance("monitor_0")
 print(instance.getSize())
-length,width = instance.getSize()
 mst,mstc,msb,msbu = advapi.createScreen(instance)
 paddle1 = 1
 paddle2 = 1
 function drawTitleScreen()
-	mstc,mst,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,10,5,5,20,colors.white,false)
-	mstc,mst,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,1,1,"Pong By IntelX",colors.blue,colors.black)
-	mstc,mst,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,20,22,"Click the White Button to Start!",colors.blue,colors.black)
-	advapi.updateScreen(instance,mst,mstc,msb,msbu)
+	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,10,5,5,20,colors.white,false)
+	mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,1,1,"Pong By IntelX",colors.blue,colors.black)
+	mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,20,22,"Click the White Button to Start!",colors.blue,colors.black)
+	advapi.updateScreen(instance,mst,mstc,msb)
 end
 function drawScreen(p1,p2,bx,by)
+	local length,width
 	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,1,p1,3,p1+5,colors.white,false)
 	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,length -3, p2, length, p2 + 5, colors.yellow,false)
 	
-	advapi.updateScreen(instance,mst,mstc,msb,msbu)
+	advapi.updateScreen(instance,mst,mstc,msb)
 	return mst,mstc,msb,msbu
 end
