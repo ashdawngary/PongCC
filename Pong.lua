d7994d8fae6b0d833d19922801dbe5f6a2403431
ashdawngary <ashdawngary@gmail.com>
2017-07-24 14:12:31 -0400

    testing coinflip

--- Pong.lua
+++ Pong.lua
@@ -1,10 +1,16 @@
--- Pong for LUA
+-- Pong for LUA 82 wide 33 down
+-- Mid is 41, 16
 os.loadAPI("advapi")
+
+
 instance = advapi.getMonitorInstance("monitor_5")
 print(instance.getSize())
 mst,mstc,msb,msbu = advapi.createScreen(instance)
 paddle1 = 1
 paddle2 = 1
+function dist(x1,y1,x2,y2)
+	return sqrt(abs(x1-x2)^2 + abs(y2-y1)^2)
+end
 function drawTitleScreen()
 	while true do
 		mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,5,5,20,10,colors.white,false)
@@ -18,6 +24,7 @@ end
 function TitleScreenAnimation()
 	for offset = 0,30 do
 		print("Rendering Frame #"..offset);
+		mst,mstc,msb,msbu = advapi.createScreen(instance)
 		mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,5-offset,5,20-offset,10,colors.white,false)
 		mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,10-offset,7,"START",colors.white,colors.yellow)
 		mst,mstc,msb,msbu = advapi.writeText(instance,mst,mstc,msb,msbu,1-offset,1,"Pong By IntelX",colors.yellow,colors.black)
@@ -32,7 +39,7 @@ function drawScreen(p1,p2,bx,by)
 	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,1,p1,3,p1+5,colors.white,false)
 	mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,length -3, p2, length, p2 + 5, colors.yellow,false)
 	
-	advapi.updateScreen(instance,mst,mstc,msb)
+	
 	return mst,mstc,msb,msbu
 end
 function waitforstart()
@@ -51,6 +58,49 @@ function waitforstart()
 	
 	
 end
+function coinFlipstart()
+	while true do -- Detect Coin Hit.
+	
+	local event, side, x, y = os.pullEvent("monitor_touch")
+	print(x.."  and "..y)
+	if (dist(x,y,41,16) < 5) then
+			print("Coin was hit!")
+			return 1
+			
+		end
+	
+	end
+	
+end
+function CoinFlipBackground()
+	mst,mstc,msb,msbu = advapi.createScreen(instance)
+	paddle1 = 1
+	paddle2 = 1
+	target_paddle1 = random.randint(1,70)
+	target_paddle2 = random.randint(1,70)
+	while true do
+	mst,mstc,msb,msbu = advapi.createScreen(instance)
+		drawScreen(paddle1,paddle2,0,0) -- no ball yet.
+		if (paddle1  == target_paddle1) then -- extremely slick way to animate kek.
+			target_paddle1 = random.randint(1,70)
+		else
+			paddle1 += (target_paddle1 - paddle1) / (abs(target_paddle1-paddle1))
+		end
+		if (paddle2  == target_paddle2) then -- extremely slick way to animate kek.
+			target_paddle2 = random.randint(1,70)
+		else
+			paddle2 += (target_paddle2 - paddle2) / (abs(target_paddle2-paddle2))
+		end
+		mst,mstc,msb,msbu = advapi.drawRect(instance,mst,mstc,msb,msbu,43,14,47,18,colors.orange,false)
+		advapi.updateScreen(instance,mst,mstc,msb)
+		sleep(0.2) -- Solid 5fps.
+	end
+end
 parallel.waitForAny(waitforstart,drawTitleScreen)
 TitleScreenAnimation()
 print("Passed Title Screen! Loading Code.")
+parallel.waitForAny(coinFlipStart,CoinFlipBackground)
+while true do
+	
+
+end
